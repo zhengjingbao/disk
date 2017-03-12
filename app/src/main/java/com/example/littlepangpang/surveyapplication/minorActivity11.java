767506aed50a124f6a4e2a35190873c422609923
zhengjingbao <1952606176@qq.com>
2017-03-12 15:36:50 +0800

    second

---
+++ app/src/main/java/com/example/littlepangpang/surveyapplication/minorActivity11.java
@@ -0,0 +1,244 @@
+package com.example.littlepangpang.surveyapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.*;
+import android.os.Environment;
+import android.util.Log;
+import android.Manifest;
+
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.String;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class minorActivity11 extends AppCompatActivity {
+
+
+    private TextView Tview = null;
+    private RadioButton Rbutton = null;
+    private CheckBox checkBox = null;
+    private Button btn = null;
+    private ListView LView = null;
+    private RadioGroup genderGroup = null;
+    private EditText Etext = null;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    /**
+     * Checks if the app has permission to write to device storage
+     * If the app does not has permission then the user will be prompted to
+     * grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+// Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+// We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minor11);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       /* setSupportActionBar(toolbar);*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void OnClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            });*/
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void button_click1(View view) throws IOException {
+
+
+        Tview = (TextView) findViewById(R.id.textviewminor11);
+
+        MainActivity.stringList[MainActivity.I] = Tview.getText().toString();
+        MainActivity.I++;
+        Etext = (EditText) findViewById(R.id.edittextminor11);
+
+        MainActivity.stringList[MainActivity.I] = Etext.getText().toString();
+        String filePath = "/sdcard/";
+        String fileName = "log.txt";
+        Tview.setText("thank you");
+        String database = MainActivity.stringList[0];
+        minorActivity11.verifyStoragePermissions(this);
+        for (int i = 0; i <= MainActivity.I; i++)
+          writeTxtToFile(MainActivity.stringList[i],filePath,fileName);
+
+
+    }
+
+    public void writeTxtToFile(String strcontent, String filePath, String fileName) {
+        //生成文件夹之后，再生成文件，不然会出错
+        makeFilePath(filePath, fileName);
+
+        String strFilePath = filePath+fileName;
+        // 每次写入时，都换行写
+        String strContent = strcontent + "\r\n";
+        try {
+            File file = new File(strFilePath);
+            if (!file.exists()) {
+                Log.d("TestFile", "Create the file:" + strFilePath);
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+            RandomAccessFile raf = new RandomAccessFile(file, "rwd");
+            raf.seek(file.length());
+            raf.write(strContent.getBytes());
+            raf.close();
+        } catch (Exception e) {
+            Log.e("TestFile", "Error on write File:" + e);
+        }
+    }
+
+    // 生成文件
+    public File makeFilePath(String filePath, String fileName) {
+        File file = null;
+        makeRootDirectory(filePath);
+        try {
+            file = new File(filePath + fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    // 生成文件夹
+    public static void makeRootDirectory(String filePath) {
+        File file = null;
+        try {
+            file = new File(filePath);
+            if (!file.exists()) {
+                file.mkdir();
+            }
+        } catch (Exception e) {
+            Log.i("error:", e+"");
+        }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("minorActivity11 Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/littlepangpang/surveyapplication/minorActivity4.java
@@ -0,0 +1,114 @@
+package com.example.littlepangpang.surveyapplication;
+
+/**
+ * Created by Little PangPang on 2017/3/10.
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.*;
+
+public class minorActivity4 extends AppCompatActivity {
+    private RadioGroup genderGroup = null;
+    private RadioButton Rbutton1 = null;
+    private RadioButton Rbutton2 = null;
+    private RadioButton Rbutton3 = null;
+    private Button btn = null;
+    private TextView Tview=null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minor4);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void OnClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            });*/
+
+
+        genderGroup = (RadioGroup) findViewById(R.id.radiobtngroupminor4);
+        Rbutton1 = (RadioButton) findViewById(R.id.radiobtn1minor4);
+        Rbutton2 = (RadioButton) findViewById(R.id.radiobtn2minor4);
+        Rbutton3 = (RadioButton) findViewById(R.id.radiobtn3minor4);
+        btn = (Button) findViewById(R.id.btnminor4);
+        //给RadioGroup设置事件监听
+        genderGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radiogroup, int checkedId) {
+
+                if (Rbutton1.isChecked() == true)
+                {
+                    MainActivity.stringList[MainActivity.I]=Rbutton1.getText().toString();
+                    MainActivity.I++;
+                    btn.setText("next");
+                }
+                if( Rbutton2.isChecked() == true)
+                {
+                    MainActivity.stringList[MainActivity.I]=Rbutton2.getText().toString();
+                    MainActivity.I++;
+                    btn.setText("next");
+                }
+                  if( Rbutton3.isChecked() == true)
+                  {
+                      MainActivity.stringList[MainActivity.I]=Rbutton3.getText().toString();
+                      MainActivity.I++;
+                      btn.setText("next");
+                  }
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void button_click1(View view) {
+        Tview=(TextView)findViewById(R.id.textviewminor4);
+        MainActivity.stringList[MainActivity.I]=Tview.getText().toString();
+        MainActivity.I++;
+
+        btn=(Button)findViewById(R.id.btnminor4);
+        if(btn.getText()=="next") {
+            startActivity(new Intent(this, minorActivity5.class));
+        }
+
+
+    }
+}
